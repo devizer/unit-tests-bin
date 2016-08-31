db9e68bf6b4c46eee7253f0bc0f26c5a6a10df1c
devizer <vgoyda@gmail.com>
2016-09-01 00:29:59 +0300

    sync

--- Universe.MediaProbe(non-windows).sh
+++ Universe.MediaProbe(non-windows).sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-work=$HOME/.test
+work=/d2/.tests
 app=Universe.MediaProbe
 rm -rf $work/$app
 mkdir -p $work/$app
 rm -f $work/.$app.7z
-wget --no-check-certificates -O $work/$app 'https://raw.githubusercontent.com/devizer/unit-tests-bin/master/Universe.MediaProbe(non-windows).7z'
+wget --no-check-certificate -O $work/.$app 'https://raw.githubusercontent.com/devizer/unit-tests-bin/master/Universe.MediaProbe(non-windows).7z'
+7za x -y -o$work $work/.$app
+cd $work && cd 'Universe.MediaProbe(non-windows)'
+pdb2mdb Universe.MediaProbe.Tests.dll 
+pdb2mdb Universe.MediaProbe.dll 
+
+nu-c Universe.MediaProbe.Tests.dll 
+
+
+
